881c4fe38dc9f05007b0a354a89b1904065df48b
SachiaTheRaven <43549644+SachiaTheRaven@users.noreply.github.com>
2020-10-13 13:35:40 +0200

    Timer + win option
    implemented a timer, the player wins if they survive until the timer runs out

---
+++ Assets/Scripts/GameManager/Timer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace RTSGame
+{
+    public class Timer : MonoBehaviour
+    {
+        public int currentSeconds = 0;
+        private int timeLimit = 60;
+        public TextMeshProUGUI timerText;
+        void Start()
+        {
+            StartCoroutine("Tick");
+        }
+
+
+        private IEnumerator Tick()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(1);
+                currentSeconds++;
+                DisplayTime();
+                if (currentSeconds >= timeLimit)
+                {
+                    FindObjectOfType<GameController>().CurrentGameState = GameState.WON;
+
+                }
+            }
+
+        }
+        private void DisplayTime()
+        {
+            timerText.text = currentSeconds / 60 + ":" + currentSeconds % 60;
+        }
+    }
+}
